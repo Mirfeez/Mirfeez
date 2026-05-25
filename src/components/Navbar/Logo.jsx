b342cd6c590324cc8401e73c06b40702dc532792
Mirfeez <mahammadmirfeez@gmail.com>
2026-05-25 20:24:30 +0530

    changed logo and added one icon image for home

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-
 const Logo = () => {
-   return (
+	return (
 		<div
 			id="logo"
-			className="text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl font-semibold "
+			className="flex items-center text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl font-semibold"
 		>
-			<span>&lt;</span>
-			<span className="text-blue-400">Mirfeez</span>
-			<span> /&gt;</span>
+			<span className="text-green-400">M</span>
+			<span className="text-blue-400">i</span>
+			<span className="text-blue-400">r</span>
+			<span className="text-blue-400">f</span>
+			<span className="text-blue-400">e</span>
+			<span className="-m-0.5 text-xm">🌍</span>
+			<span className="text-blue-400">z</span>
 		</div>
 	);
-}
+};
 
 export default Logo;
--- src/pages/home/LeftSide.jsx
+++ src/pages/home/LeftSide.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion as Motion } from 'motion/react';
 import { Link } from 'react-router-dom';
 import { HashLink } from "react-router-hash-link";
-
+import bed from '../../components/Navbar/bed.png'
 
 const LeftSide = () => {
    return (
@@ -10,21 +10,23 @@ const LeftSide = () => {
 			{/* HERO */}
 			<section className="flex flex-col justify-center items-center text-center px-6">
 				<Motion.h1
-					animate={{
-						y: 0,
-						opacity: 1,
-					}}
-					initial={{
-						y: -50,
-						opacity: 0,
-					}}
-					transition={{
-						delay: 0.5,
-						duration: 1,
-					}}
-					className="text-4xl md:text-6xl font-bold mb-4"
+					initial={{ y: -40, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ delay: 0.3, duration: 0.8 }}
+					className="flex flex-wrap items-center justify-center gap-2 text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight"
 				>
-					Hi, I’m <span className="text-blue-400">Mirfeez</span> 👋
+					<span className="text-white">Hi, I’m</span>
+
+					<span className="flex items-center gap-2">
+						<span className="text-blue-400">Mirfeez</span>
+
+						<img
+							
+							src={bed}
+							alt="Mirfeez icon"
+							className="w-20 h-20 sm:w-24 sm:h-24 md:w-26 md:h-26 object-contain drop-shadow-[0_0_10px_rgba(59,130,246,0.6)] scale-250"
+						/>
+					</span>
 				</Motion.h1>
 
 				<Motion.h2
@@ -67,10 +69,12 @@ const LeftSide = () => {
 				</Motion.p>
 
 				<div className="flex gap-4">
-					<Link to='/works' className="px-6 py-3 bg-blue-400 text-black rounded-lg font-bold hover:bg-black transition hover:text-blue-400 hover:border-2 hover:border-blue-400 hover:shadow-md hover:shadow-blue-400">
+					<Link
+						to="/works"
+						className="px-6 py-3 bg-blue-400 text-black rounded-lg font-bold hover:bg-black transition hover:text-blue-400 hover:border-2 hover:border-blue-400 hover:shadow-md hover:shadow-blue-400"
+					>
 						View My Work
 					</Link>
-					
 				</div>
 			</section>
 
