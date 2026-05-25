0a325ac46e7b2fafb6c19be87edc1d0615bdf06f
Mirfeez <mahammadmirfeez@gmail.com>
2026-05-25 13:47:27 +0530

    okk

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/home/Home";
-import About from "./pages/about/About";
+import About from "./pages/About/About";
 import Contact from "./pages/Contact";
 import Notfound from "./pages/Notfound";
 import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Works from "./pages/work/Works";
+import { useEffect } from "react";
 
 const App = () => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+	}, [pathname]);
+
 	return (
 		<>
 			<Navbar />
